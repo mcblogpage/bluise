1ebc3357db2d84e3b74fdad16d8ea61bb5428e8e
mcblogpage <107860129+mcblogpage@users.noreply.github.com>
2023-03-22 15:01:02 +0530

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -37,7 +37,7 @@
         <img
           alt="Hero"
           class="rounded shadow-xl"
-          src="https://source.unsplash.com/random/720x400"
+          src="/images/uploads/austin-distel-gUIJ0YszPig-unsplash.jpg"
         />
       </div>
     </div>
